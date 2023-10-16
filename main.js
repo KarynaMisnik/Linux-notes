45a256b0477abe3443ea0a019dbc2a7a789af6c7
KarynaMisnik <karinamisnik94@gmail.com>
2023-10-16 19:25:38 +0300

    .
    Signed-off-by: KarynaMisnik <karinamisnik94@gmail.com>

---
+++ main.js
@@ -0,0 +1,19 @@
+window.onload = function () {
+  document.getElementById("download").addEventListener("click", () => {
+    const resume = this.document.getElementById("resume");
+
+    var opt = {
+      filename: "resume.pdf",
+      image: { type: "pdf", quality: 0.98 },
+      html2canvas: { scale: 2 },
+      jsPDF: {
+        unit: "in",
+        format: "a4",
+        orientation: "portrait",
+        fontSize: "12",
+        lineHeight: "1.15",
+      },
+    };
+    html2pdf().from(resume).set(opt).save();
+  });
+};
